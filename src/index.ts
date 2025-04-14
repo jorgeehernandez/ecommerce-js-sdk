b09f408c64bb4b1d1b0f117e9c5fc7ea917c14f7
Jorge Hernandez <jorgeehernandez89@gmail.com>
2025-04-14 18:09:47 -0500

    get dynamicHome query implementation in progress

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import Platform
+    from "./core/Platform";
+import HomeFilter
+    from "./core/services/home/HomeFilter";
+
+
+(async function fetchHome() {
+    const platform = new Platform('https://nextgentheadless.instaleap.io/api/v3', 'D1');
+
+    const homeFilter = new HomeFilter()
+        .byStore('11808')
+        .byPlatform('WEB')
+        .byScreenSize('large');
+
+
+    const home = await platform.homeService.home(homeFilter);
+
+})();
 
