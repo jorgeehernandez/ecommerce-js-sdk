dbaee4d48621456f361ad65fcf8a7dd8ab24fa70
Jorge Hernandez <jorgeehernandez89@gmail.com>
2025-04-15 12:02:44 -0500

    add a javascrip-ism to the way of developer gonna use the sdk

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,21 @@ import Platform
 import HomeFilter
     from "./core/services/home/HomeFilter";
 
-
+// Example : GetHome
 (async function fetchHome() {
-    const platform = new Platform('https://nextgentheadless.instaleap.io/api/v3', 'D1');
 
-    const homeFilter = new HomeFilter()
-        .byStore('11808')
-        .byPlatform('WEB')
-        .byScreenSize('large');
+    const platform = new Platform({
+        baseUrl: 'https://nextgentheadless.instaleap.io/api/v3',
+        clientId: 'D1'
+    });
 
+    const homeFilter = new HomeFilter({
+        byStore: '11808',
+        byPlatform: 'WEB',
+        byScreenSize: 'large'
+    })
 
-    const home = await platform.homeService.home(homeFilter);
+    const home = await platform.homeService.home({filter: homeFilter});
 
 })();
 
